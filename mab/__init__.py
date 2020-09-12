ac11be63557777a72ebf52a99e693a87abc28004
Alexey <butirev@gmail.com>
2020-09-12 16:34:15 -0400

    fix imoport for libraries

--- mab/__init__.py
+++ mab/__init__.py
@@ -1,4 +1,14 @@
-#__init__.py
+# __init__.py
 
 __version__ = "0.1.1"
 
+from mab.montecarlo import test_alorythm
+from mab.rewards import BernoulliArm
+from mab.rewards import UniformArm
+from mab.epsilongreedy import EpsilonGreedy
+from mab.randomselect import RandomSelect
+from mab.annealingsoftmax import AnnealingSoftmax
+from mab.metric import Metric
+from mab.ab import AB
+from mab.ucb1 import UCB1
+from mab.softmax import Softmax
