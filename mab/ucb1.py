92e36cb9efe70242d6e8ade12e27affe11b7d0b1
Alexey <butirev@gmail.com>
2020-09-12 16:34:40 -0400

    fix naming

--- mab/ucb1.py
+++ mab/ucb1.py
@@ -3,13 +3,21 @@ from .mab import MAB
 from math import log, sqrt
 
 
-class UCB(MAB):
+class UCB1(MAB):
+    """Upper confidence Boundary MAB Algorythm"""
+
     def __init__(self, counts=None, values=None, n_arms=None):
+        """
+        Args:
+            counts ([int], optional): [description]. Defaults to None.
+            values ([int], optional): [description]. Defaults to None.
+            n_arms ([int], optional): [description]. Defaults to None.
+        """
         super().__init__(counts, values, n_arms)
 
     @property
     def name(self):
-        return "UCB"
+        return "UCB1"
 
     def select_arm(self):
         # observe arms with no counts
@@ -22,7 +30,9 @@ class UCB(MAB):
         total_counts = sum(self.counts)
 
         for arm in range(self.n_arms):
-            curr_value = self.values[arm] + sqrt(2*log(total_counts) / float(self.counts[arm]))
+            curr_value = self.values[arm] + sqrt(
+                2 * log(total_counts) / float(self.counts[arm])
+            )
             if curr_value > max_val:
                 max_val = curr_value
                 max_ind = arm
