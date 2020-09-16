4d9e7723affe5b89ca4e32a1018c79a5ad433973
Alexey <butirev@gmail.com>
2020-09-16 19:42:43 -0400

    add comments

--- mab/montecarlo.py
+++ mab/montecarlo.py
@@ -1,9 +1,20 @@
 def test_alorythm(algo, arms, num_sims, horizon):
-    chosen_arms = [0.0 for i in range(num_sims*horizon)]
-    rewards = [0.0 for i in range(num_sims*horizon)]
-    cumulative_rewards = [0.0 for i in range(num_sims*horizon)]
-    sim_nums = [0.0 for i in range(num_sims*horizon)]
-    times = [0.0 for i in range(num_sims*horizon)]
+    """Monte Carlo simulation of MAB malgorythm
+
+    Args:
+        algo (MAB): One of MAB alogorythms
+        arms (RewardClass): List with arms giving the rewards
+        num_sims (int): Number of simulations
+        horizon (int): Time horizon (max t)
+
+    Returns:
+        [dict]: simulation, times, chosesen arms, rewards, cummulative rewards
+    """
+    chosen_arms = [0.0 for i in range(num_sims * horizon)]
+    rewards = [0.0 for i in range(num_sims * horizon)]
+    cumulative_rewards = [0.0 for i in range(num_sims * horizon)]
+    sim_nums = [0.0 for i in range(num_sims * horizon)]
+    times = [0.0 for i in range(num_sims * horizon)]
 
     for sim in range(num_sims):
         algo.reset()
@@ -20,12 +31,14 @@ def test_alorythm(algo, arms, num_sims, horizon):
             if t == 0:
                 cumulative_rewards[index] = reward
             else:
-                cumulative_rewards[index] = cumulative_rewards[index-1] + reward
+                cumulative_rewards[index] = cumulative_rewards[index - 1] + reward
 
             algo.update(chosen_arm, reward)
 
-    return {'sim_nums': sim_nums,
-            'times': times,
-            'chosen_arms': chosen_arms,
-            'rewards': rewards,
-            'cumulative_rewards': cumulative_rewards}
+    return {
+        "sim_nums": sim_nums,
+        "times": times,
+        "chosen_arms": chosen_arms,
+        "rewards": rewards,
+        "cumulative_rewards": cumulative_rewards,
+    }
